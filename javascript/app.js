d0817791fac04470499446a7d481a4c49536eaa9
Seryozha Baleyko <seryozha.baleyko@gmail.com>
2019-12-26 17:51:51 +0300

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,16 @@
+import { progressScroll } from './progress.js'
 
+const scrollToButton = document.getElementById('top')
+
+window.addEventListener('scroll', () => {
+    progressScroll()
+    window.scrollY > 150 ? scrollToButton.style.visibility = 'visible' : scrollToButton.style.visibility = 'hidden'
+})
+
+scrollToButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    })
+})
