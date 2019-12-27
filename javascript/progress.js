2e5ace460e8d9af060d491d943f8416d14e4ad27
Серёжа Балейко <seryozha.baleyko@gmail.com>
2019-12-27 21:10:02 +0300

    Update progress.js

--- javascript/progress.js
+++ javascript/progress.js
@@ -1,7 +1,7 @@
 const progress = document.getElementById("progress__bar")
-const scrollMaxY = window.scrollMaxY || document.documentElement.scrollHeight - document.documentElement.clientHeight
 
 export function progressScroll(scrollY) {
+    const scrollMaxY = window.scrollMaxY || document.documentElement.scrollHeight - document.documentElement.clientHeight
     const percent = (scrollY / scrollMaxY) * 100
     progress.setAttribute('style', `width: ${percent}%`)
 }
