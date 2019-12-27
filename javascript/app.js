87ff166fa50cf4edcea66b6ae2c4aaffc6b314f4
Серёжа Балейко <seryozha.baleyko@gmail.com>
2019-12-27 15:31:52 +0300

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -3,9 +3,10 @@ import { progressScroll } from './progress.js'
 const scrollToButton = document.getElementById('top')
 
 window.addEventListener('scroll', () => {
-    progressScroll()
-    // window.pageYOffset
-    window.scrollY > 150 ? scrollToButton.style.visibility = 'visible' : scrollToButton.style.visibility = 'hidden'
+    const maxLengthScrollY = window.scrollMaxY || document.documentElement.scrollHeight - document.documentElement.clientHeight
+    // 'scrollMaxY' in window ? maxLengthScrollY = window.scrollMaxY : maxLengthScrollY = document.documentElement.scrollHeight - document.documentElement.clientHeight
+    progressScroll(maxLengthScrollY)
+    maxLengthScrollY > 150 ? scrollToButton.style.visibility = 'visible' : scrollToButton.style.visibility = 'hidden'
 })
 
 scrollToButton.addEventListener('click', () => {
